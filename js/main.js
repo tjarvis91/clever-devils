8d4dcd79f9fcd818677182d8ad27779b9017d30c
Tabitha Jarvis <tabitha.jarvis@garmin.com>
2016-04-12 17:34:09 -0500

    Adding menu.  Slight interactivity present.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 'use strict';
-$('#test').hide();
+var main = function() {
+  $('.menu-btn').click(function() {
+    $('.menu-btn').children('.dropdown-menu').hide();
+    $(this).children('.dropdown-menu').show();
+  });
+};
+
+$(document).ready(main);
