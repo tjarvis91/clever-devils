e1dc5b8b7e14b54e8dffa736ca7e6231111c7900
Tabitha Jarvis <tabitha.jarvis@garmin.com>
2016-04-13 17:25:23 -0500

    Abusing Bootstrap for Carousel and Menu options.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 'use strict';
 var main = function() {
-  $('.menu-btn').click(function() {
-    $('.menu-btn').children('.dropdown-menu').hide();
-    $(this).children('.dropdown-menu').slideDown(100);
+  $('#gamesCarousel').carousel();
+  $('#gamesCarousel').click(function() {
+    $('#gamesCarousel').carousel('next');
   });
+
+  $('.left').click(function(){
+    $('#gamesCarousel').carousel('prev');
+  });
+
+//  $('.dropdown-toggle').dropdown('toggle');
+  //$('.menu-btn').click(function() {
+//    $('.menu-btn').children('.dropdown-menu').hide();
+//    $(this).children('.dropdown-menu').slideDown(100);
+  //});
 };
 
 $(document).ready(main);
