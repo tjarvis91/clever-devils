f25c8f5779631805f49b293713c7ebb9befbfd80
Tabitha Jarvis <tabitha.jarvis@garmin.com>
2016-04-14 17:14:27 -0500

    Making menu fixed-top and making menu and carousel full-width.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 'use strict';
 var main = function() {
-  $('#gamesCarousel').carousel();
-  $('#gamesCarousel').click(function() {
-    $('#gamesCarousel').carousel('next');
-  });
 
-  $('.left').click(function(){
-    $('#gamesCarousel').carousel('prev');
-  });
-
-//  $('.dropdown-toggle').dropdown('toggle');
-  //$('.menu-btn').click(function() {
-//    $('.menu-btn').children('.dropdown-menu').hide();
-//    $(this).children('.dropdown-menu').slideDown(100);
-  //});
 };
 
 $(document).ready(main);
