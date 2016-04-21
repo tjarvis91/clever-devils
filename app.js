e657fafd85c7d90b5db94204f87f91bf33872e11
Tabitha Jarvis <tabitha.jarvis@garmin.com>
2016-04-21 14:10:27 -0500

    Massive update of application types.

---
+++ app.js
@@ -0,0 +1,25 @@
+/*eslint-node*/
+/*
+var jquery = require('jquery');
+var express = require('express');
+//var routes = require('./routes');
+var http = require('http');
+var bootstrap = require('bootstrap');
+var path = require('path');
+var fs = require('fs');
+var AWS = require('aws-sdk');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req,res) {
+  res.sendFile(path.join(__dirname + '/templates/index.html'));
+  console.log("Accessing Home page.");
+});
+
+app.get('/about', function(req,res) {
+  res.sendFile(path.join(__dirname + '/templates/about.html'));
+  console.log("Accessing About page.");
+})
+
+app.listen(port);
+console.log("Starting server on port " + port + ".");*/
---
+++ application.py
@@ -0,0 +1,37 @@
+# Author: Gouthaman Balaraman
+# http://gouthamanbalaraman.com/minimal-flask-login-example.html
+
+from functools import wraps
+from flask import Flask, Response, request, render_template
+
+app = Flask(__name__)
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'cdg' and password == 'secretspypassword'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route("/")
+@requires_auth
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
